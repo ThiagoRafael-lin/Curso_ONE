de490bd329ddbf2a3e617ac61f2baf0a63906446
Thiago <thiagorafael2005@gmail.com>
2024-01-25 22:07:26 -0300

    Uso de funções amplificado

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,12 +1,31 @@
 /*document = entrar no HTML, querySelector busca um elemento do HTML,
 ('h1) = indica o elemento que você quer manipular*/
-let titulo = document.querySelector('h1');
-//chama a variavel .innerHTML = é usado para colocar o valor
-titulo.innerHTML = "Jogo do número secreto";
+// let titulo = document.querySelector('h1');
+// //chama a variavel .innerHTML = é usado para colocar o valor
+// titulo.innerHTML = "Jogo do número secreto";
 
-let paragrafo = document.querySelector('p')
-paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+// let paragrafo = document.querySelector('p')
+// paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+
+
+
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exbirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    CSSMathProduct.innerHTML = texto;
+}
+
+exbirTextoNaTela("tag", "Jogo do número secreto"),
+exbirTextoNaTela("p", "Escolha um número entre 1 e 10")
 
 function verificarChute() {
-    console.log("o botão foi clicado");
+    let chute = document.querySelector('input').value
+    console.log(chute == numeroSecreto);
 }
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+
